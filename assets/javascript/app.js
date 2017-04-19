509faccb74b44f30e111c6fd16023003f0336c72
nhuyp <ypham87@gmail.com>
2017-04-18 20:40:39 -0700

    Pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+// Start button - click to start game. 
+
+// Trivia question - display only one question at a time.
+
+// Timer for each question.
+
+// Link decoration for each answer on hover. 
+
+// Answer selection - choose answer (onclick)
+	// Correct, display corresponding picture. 
+	// If incorrect, display the correct response with the corresponding picture. 
+	// If out of time, display correct response with the corresponding picture.
+
+// After user answers the question correctly, incorrectly, or times out, automatically move onto the next question (no click or keypress required). 
+
+// End of game
+	// Display number of correct, incorrect, and timed out questions.  Timer stops running.
+	// Automatically reset game without page reload. 
